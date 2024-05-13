e564c0f0945876595b409085795de2793b3f0ad4
MrSahilShaikh <shaikhsahils2000@gmail.com>
2024-05-14 00:14:59 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+import ContributionDetails from './new'
+// import Add from './Address'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+      <h1>Contribution Details</h1>
+      <ContributionDetails/>
+      {/* <Add/> */}
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App
